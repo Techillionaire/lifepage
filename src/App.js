6fb383f46d40c465240de5f6145aacc1a48b82b1
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-06-13 20:08:43 +0100

    Login.jsx page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Register from './Pages/Register'
+import Login from './Pages/Login'
 
 
 const App = () => {
   return (
     <div>
-      <Register/>
+      <Login/>
     </div>
   )
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,51 +2,63 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='container'>
-        <form class="row g-3">
-  <div class="col-md-6">
-    <label for="inputEmail4" class="form-label">Email</label>
-    <input type="email" class="form-control" id="inputEmail4"/>
-  </div>
-  <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4"/>
-  </div>
-  <div class="col-12">
-    <label for="inputAddress" class="form-label">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
-  </div>
-  <div class="col-12">
-    <label for="inputAddress2" class="form-label">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
-  </div>
-  <div class="col-md-6">
-    <label for="inputCity" class="form-label">City</label>
-    <input type="text" class="form-control" id="inputCity"/>
-  </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="inputZip"/>
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"/>
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Sign in</button>
-  </div>
-</form>
+    <div class="container text-center m-5">
+        <div class="row justify-content-center ">
+            <div class="col-4 bg-primary text-white shadow">
+                <div className="row m-5">
+                <h1 className='fs-5'>Welcome back!</h1>
+                </div>
+                <div className="row ">
+                <p>Don't have an account? <a href="#">Register now</a> </p>
+                </div>
+               
+               
+            </div>
+            <div class="col-4 bg-dark  shadow">
+                <form className='align-items-center m-5 text-white justify-content-center'>
+                    <img src="./assets/logo.png" className='w-25 m-5' alt=""/>
+                    <div class="row mb-3">
+                        {/* <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label> */}
+                        <div class="col-sm-10 ms-2">
+                        <input type="email" class="form-control" id="inputEmail" placeholder='Email'/>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        {/* <label for="inputPassword" class="col-sm-2 col-form-label">Password</label> */}
+                        <div class="col-sm-10 ms-2">
+                        <input type="password" class="form-control" id="inputPassword" placeholder='Password'/>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-8 offset-sm-2 ms-2">
+                        <div class="form-check text-center">
+                            <input class="form-check-input" type="checkbox" id="gridCheck1"/>
+                            <label class="form-check-label" for="gridCheck1">
+                           Remember me
+                            </label>
+                        </div>
+                        </div>
+
+                    </div>
+                    
+                    <button type="submit" class="btn w-75 btn-primary m-3">Register</button>
+
+                    <div class="row mb-3">
+
+                        <div class="col-sm-8 ms-5 offset-sm-2 ">
+                            <a href="#">Forgot password?</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            
+        </div>
+
+
+
+
+
+       
     </div>
   )
 }
