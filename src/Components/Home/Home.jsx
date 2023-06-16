8cbc188537cc8614d5164a19d6217917b472a74b
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-06-16 11:13:19 +0100

    Homepage.jsx and Mainpage.jsx created

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar'
+import Mainpage from '../Mainpage/Mainpage'
 
 const Home = () => {
   return (
     <>
+    <div>
         <Navbar/>
+        <Mainpage/>
+    </div>
     </>
   )
 }
