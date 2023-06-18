6c1b886ad8c07b1cd0c3b693695209f540f8b6e1
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-06-18 11:36:19 +0100

    Login, Register, Home and Products routes defined

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import Register from './Pages/Register'
 import Login from './Pages/Login'
 import Home from './Components/Home/Home'
 import Products from './Components/Products/Products'
+import { Routes, Route } from 'react-router-dom'
 
 
 const App = () => {
   return (
-    <>
-      <Products/>
-    </>
+    
+     <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/products" element={<Products/>} />
+
+     </Routes>
+     
+    
   )
 }
 
