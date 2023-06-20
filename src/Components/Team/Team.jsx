f783fdc8a9342eef8b2da86f9b5d79a44947c4af
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-06-20 17:36:52 +0100

    Team.jsx  component created

---
+++ src/Components/Team/Team.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Team = () => {
+  return (
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Level</th>
+      <th scope="col">Date</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Mr Jonathan</td>
+      <td>Jonathanjotham@gmail.com</td>
+      <td>Business Associate</td>
+      <td>June 19, 2023</td>
+    </tr>
+    <tr>
+      <td>Juwon Omoboye</td>
+      <td>juwonomoboye@gmail.com</td>
+      <td>Company Advocate</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>Tony Njoku</td>
+      <td>Tonynjoku@gmail.com</td>
+      <td>Business Associate</td>
+      <td>June 19, 2023</td>
+    </tr>
+  </tbody>
+</table>
+  )
+}
+
+export default Team
