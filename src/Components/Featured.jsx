383aaaef317c1fe3f1b6c67b371973311ab31dda
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-06-20 16:55:18 +0100

    UI updated for Featured and Mainpage Components

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -4,8 +4,8 @@ import {FaLocationArrow, FaMoneyBill} from 'react-icons/fa'
 
 const Featured = () => {
   return (
-    <div className='container p-5'>
-        <h3>Featured Products</h3>
+    <div className='container p-5 '>
+        <h3 className='text-center'>Featured Products</h3>
         <div class="row row-cols-1 mb-5 row-cols-md-3 g-4">
                     <div class="col">
                         <div class="card h-100">
--- src/Components/Mainpage/Mainpage.jsx
+++ src/Components/Mainpage/Mainpage.jsx
@@ -1,43 +1,50 @@
 import React from 'react'
+import {FaLandmark, FaMoneyBill } from 'react-icons/fa'
+import {RiTeamFill} from 'react-icons/ri'
+import {GiCash} from 'react-icons/gi'
+import { whitesmoke } from '@mui/material/colors'
 
 const Mainpage = () => {
+
+
+
   return (
-    <div className='container'>
-        <h1>Portfolio Overview</h1>
-            <div class="row row-cols-1 row-cols-md-2 g-4">
-                <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="..."/>
+    <div className='container justiify-content-center'>
+        <h3 className='' >Portfolio Overview</h3>
+            <div class="row g-4 justify-content-center text-center" >
+                <div class="col-6" style={{width: '18rem' }}  >
+                    <div class="card h-100" style={{backgroundColor: '#FF8839'}}>
                     <div class="card-body">
+                        <FaLandmark class=''  />
                         <h5 class="card-title">Properties Sold</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text" style={{color: 'whitesmoke' }}>4</p>
                     </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="..."/>
+                <div class="col-6" style={{width: '18rem' }}>
+                    <div class="card h-100" style={{backgroundColor: '#FF8839'}}>
                     <div class="card-body">
+                        <RiTeamFill class='' />
                         <h5 class="card-title">Recruits</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text" style={{color: 'whitesmoke' }}>25</p>
                     </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="..."/>
+                <div class="col-6" style={{width: '18rem' }}>
+                    <div class="card h-100" style={{backgroundColor: '#FF8839'}}>
                     <div class="card-body">
+                        <FaMoneyBill class='' />
                         <h5 class="card-title">Commission Overrides</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        <p class="card-text" style={{color: 'whitesmoke' }}>N3,000,000.00</p>
                     </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="..."/>
+                <div class="col-12" style={{width: '18rem' }}>
+                    <div class="card h-100" style={{backgroundColor: '#FF8839'}}>
                     <div class="card-body">
+                        <GiCash class='' />
                         <h5 class="card-title">Total Earnings</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text" style={{color: 'whitesmoke' }}>N10,000,000.00</p>
                     </div>
                     </div>
                 </div>
